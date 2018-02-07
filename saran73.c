e8849dd62ea452eeda3598653ab7168010c8301b
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-07 16:06:56 +0530

    Create saran73.c

---
+++ saran73.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+int a,b,c
+printf("enter the value");
+scanf("%d",&a);
+printf("enter the value");
+scanf("%d",&b);
+printf("enter the total value");
+scanf("%d",&c);
+getch();
+return 0;
+}
